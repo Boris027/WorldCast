6ecf597b2716abad67b90014950d28d4fc0b6e71
Flash98 <titankiller027@gmail.com>
2025-10-17 03:21:49 +0200

    fix

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -23,7 +23,11 @@ const Sidebar:React.FC<SidebarProps>=({ playlist,onClickPlaylist,visibility,side
                 <p style={{}}>{country}</p>
                 
 
-                <button type="button"  onClick={()=>sidebarVisibility("none")}>
+                <button type="button"  onClick={()=>{
+                        sidebarVisibility("none")
+                        const channelcontainer=document.getElementById("channelcontainer")
+                        channelcontainer!.innerHTML=""
+                    }}>
                     <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>

fbf61285b76d46cde525eee5d0117e53a9bdc4df
Flash98 <titankiller027@gmail.com>
2025-10-21 16:26:34 +0200

    fixed toogle touch buttons

--- src/components/TooglePanel.tsx
+++ src/components/TooglePanel.tsx
@@ -60,9 +60,9 @@ const TooglePanel:React.FC<TooglePanelProps>=({ TooglePanelchanges,mode,changeMo
 
     
 
-    return <div style={{display:"flex",flexDirection:"column",gap:"10px", position:"fixed", bottom:"20px",left:"20px",alignItems:"flex-start"}}>
+    return <div style={{display:"flex",flexDirection:"column",gap:"10px", position:"fixed", bottom:"20px",left:"20px",alignItems:"flex-start",pointerEvents:"none"}}>
 
-        <button style={{textAlign:"left",cursor:"pointer",alignSelf: "flex-start"}} onClick={c=>{
+        <button style={{textAlign:"left",cursor:"pointer",alignSelf: "flex-start",pointerEvents:"auto"}} onClick={c=>{
             setaboutsectionenabled(true)
             }}>
             <div style={{display:"flex",alignItems:"center",gap:"10px"}}>
@@ -84,7 +84,7 @@ const TooglePanel:React.FC<TooglePanelProps>=({ TooglePanelchanges,mode,changeMo
             </div>
         </button>
         
-        <label className="inline-flex items-center cursor-pointer">
+        <label className="inline-flex items-center cursor-pointer" style={{pointerEvents:"auto"}}>
             <input type="checkbox"  value="" className="sr-only peer" checked={checked} onChange={(e) => {
                 setChecked(e.target.checked)
                 SetWorldRotation(e.target.checked)
@@ -94,7 +94,7 @@ const TooglePanel:React.FC<TooglePanelProps>=({ TooglePanelchanges,mode,changeMo
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Rotating</span>
         </label>
 
-        <label className="inline-flex items-center cursor-pointer">
+        <label className="inline-flex items-center cursor-pointer" style={{pointerEvents:"auto"}}>
             <input type="checkbox"  value="" className="sr-only peer" checked={checked2} onChange={(e) => {
                 setChecked2(e.target.checked)
                 SetClouds(e.target.checked)
@@ -104,7 +104,7 @@ const TooglePanel:React.FC<TooglePanelProps>=({ TooglePanelchanges,mode,changeMo
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Clouds</span>
         </label>
 
-        <label className="inline-flex items-center cursor-pointer">
+        <label className="inline-flex items-center cursor-pointer" style={{pointerEvents:"auto"}}>
             <input disabled type="checkbox"  value="" className="sr-only peer" checked={checked3} onChange={(e) => {
                 setChecked3(e.target.checked)
                 SetTransparent(e.target.checked)
@@ -113,7 +113,7 @@ const TooglePanel:React.FC<TooglePanelProps>=({ TooglePanelchanges,mode,changeMo
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Transparent Country</span>
         </label>
 
-        <div className="w-max mx-auto mt-10" style={{margin:"auto"}}>
+        <div className="w-max mx-auto mt-10" style={{margin:"auto",pointerEvents:"auto"}} >
             <div className="flex rounded-full border border-gray-300 overflow-hidden">
                 
                 <button id="tv" className="toggle-btn px-6 py-2 bg-gray-200 text-gray-700 cursor-pointer" data-value="tv" onClick={c=>{
